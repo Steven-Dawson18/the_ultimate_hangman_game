316d3d929ef765ca1724e91a1b48f58262fd4f01
Steve <la14b@yahoo.co.uk>
2021-09-28 08:32:05 +0000

    Add random words into a function

--- run.py
+++ run.py
@@ -1,19 +1,25 @@
 import random
 
-with open("dragon_words.txt", "r") as file:
-    allText = file.read()
-    words = list(map(str, allText.split()))
-    picked = random.choice(words).upper()
-    print(picked)
+def get_random_word():
+    """
+    This function returns a random word to start the game
+    """
 
-with open("cities_words.txt", "r") as file:
-    allText = file.read()
-    words = list(map(str, allText.split()))
-    picked = random.choice(words).upper()
-    print(picked)
-
-with open("words.txt", "r") as file:
-    allText = file.read()
-    words = list(map(str, allText.split()))
-    picked = random.choice(words).upper()
-    print(picked)
+    if game_choice == "DRAGON":
+        with open("dragon_words.txt", "r") as file:
+            allText = file.read()
+            words = list(map(str, allText.split()))
+            picked = random.choice(words).upper()
+            print(picked)
+    elif game_choice == "CITIES":
+        with open("cities_words.txt", "r") as file:
+            allText = file.read()
+            words = list(map(str, allText.split()))
+            picked = random.choice(words).upper()
+             print(picked)
+    elif game_choice == "STANDARD":
+        with open("words.txt", "r") as file:
+            allText = file.read()
+            words = list(map(str, allText.split()))
+            picked = random.choice(words).upper()
+            print(picked)
