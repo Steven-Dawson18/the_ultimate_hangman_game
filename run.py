7723700a3e471ade8661657e01a8f98c97ba3582
Steve <la14b@yahoo.co.uk>
2021-09-28 08:26:25 +0000

    Add random word selection from dragon_words.txt

--- run.py
+++ run.py
@@ -1,2 +1,7 @@
 import random
 
+with open("dragon_words.txt", "r") as file:
+    allText = file.read()
+    words = list(map(str, allText.split()))
+    picked = random.choice(words).upper()
+    print(picked)
